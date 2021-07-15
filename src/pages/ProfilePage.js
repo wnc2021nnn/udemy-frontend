2b49b793bbe3f233702bb6e6fa841a409455aca1
Nhin <nhinlechi@gmail.com>
2021-07-15 22:47:17 +0700

    Update packages

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,11 +1,11 @@
+import { Container } from "@material-ui/core";
 import { Fragment } from "react";
+import { Col, Row } from "reactstrap";
 
 export default function ProfilePage(props) {
-    return (
-        <Fragment>
-            <text>
-                Hello this is profile page
-            </text>
-        </Fragment> 
-    );
+  return (
+    <Fragment>
+        <Container className="themed-container" fluid="xl">Container???</Container>
+    </Fragment>
+  );
 }
